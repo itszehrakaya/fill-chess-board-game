f275065470acabe2f1b34b46e2d2fe9768e29bfb
Zehra Kaya <72206348+thezkaya@users.noreply.github.com>
2023-02-05 22:37:56 +0300

    :/

--- queen.py
+++ queen.py
@@ -2,25 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-x, y = np.ogrid[:8, :8]
-def queen(i):
-    turn = 0
-    indexList = []
-    while turn < i:
-        turn += 1
-        location = int(input())
-        row = location//10 - 1 
-        col = location%10 - 1
-
-        findIndex = np.where((x + y == col+ row) | 
-                     (x - y == abs(col-row)) |
-                     (x == row)  | (y==col ), x, -1 )
-        
-        getIndex  = np.where(findIndex != -1)
-        indexList.append(list(zip(getIndex[0], getIndex[1])))
-
-        plt.imshow(findIndex)
-        print(indexList)
-
+def queen(t):
+  Board = np.add.outer(range(8), range(8)) %2
+  turn = 0
+  while turn < t:
+    turn += 1
+    row = int(input("row:"))
+    col = int(input("column:"))
+    for idx, i in np.ndenumerate(Board):
+      x = idx[0]
+      y = idx[1]
+      idxList = []
+      if (x + y == col+ row) or  (x - y == abs(col-row))  or (x == row) or (y==col ):
+        idxList.append(idx)
+        for a in idxList:
+          Board[a] =  -1          
+    plt.imshow(Board)
+    plt.show()
 queen(5)
 plt.show()
